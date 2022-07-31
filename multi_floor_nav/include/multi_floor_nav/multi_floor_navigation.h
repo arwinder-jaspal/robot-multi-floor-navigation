8029dd8a1145485afbbff4d58091db5eb3089bd3
arwinder-jaspal <arwinderjaspal@gmail.com>
2022-08-01 02:25:40 +0800

    added state to check with init pose successful

--- multi_floor_nav/include/multi_floor_nav/multi_floor_navigation.h
+++ multi_floor_nav/include/multi_floor_nav/multi_floor_navigation.h
@@ -5,14 +5,22 @@
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf/tf.h>
+#include <angles/angles.h>
+#include <math.h>
+
 
 class MultiFloorNav{
     private:
         enum State{
-            INIT_POSE, NAV_TO_WP_1, DONE
+            INIT_POSE,
+            CHECK_INITPOSE, 
+            NAV_TO_WP_1, 
+            DONE
         };
         State nav_state;
-        double loop_rate;
+        bool received_amcl_pose;
+        double loop_rate, max_linear_error, max_angular_error;
         ros::Publisher initial_pose_pub, goal_pub;
         ros::Subscriber amcl_pose_sub;
         geometry_msgs::PoseWithCovarianceStamped curr_pose;
@@ -21,6 +29,7 @@ class MultiFloorNav{
         void amclPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
         void init_pose(double x, double y, double z, double yaw);
         void send_simple_goal(double x, double y, double z, double yaw);
+        bool check_robot_pose(double x, double y, double yaw);
 
     public:
         MultiFloorNav();
--- multi_floor_nav/src/multi_floor_navigation.cpp
+++ multi_floor_nav/src/multi_floor_navigation.cpp
@@ -5,13 +5,18 @@ using namespace std;
 
 MultiFloorNav::MultiFloorNav(){
     nav_state = MultiFloorNav::State::INIT_POSE;
+    received_amcl_pose = false;
     loop_rate = 1.0;
+    max_angular_error = 0.0;
+    max_linear_error = 0.0;
 }
 
 void MultiFloorNav::initialize(ros::NodeHandle& n){
     ros::NodeHandle np("~");
 
-    ros::param::param<double>("~loop_rate", loop_rate, 1.0);
+    ros::param::param<double>("~loop_rate", loop_rate, 5.0);
+    ros::param::param<double>("~max_angular_error", max_angular_error, 0.05);
+    ros::param::param<double>("~max_linear_error", max_linear_error, 0.5);
 
     initial_pose_pub = n.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 1, true);
     goal_pub = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1);
@@ -20,6 +25,7 @@ void MultiFloorNav::initialize(ros::NodeHandle& n){
 }
 
 void MultiFloorNav::amclPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
+    received_amcl_pose = true;
     curr_pose.pose = msg->pose;
 }
 
@@ -51,12 +57,41 @@ void MultiFloorNav::init_pose(double x, double y, double z, double yaw){
     ROS_INFO("[Multi Floor Nav] Initializing Robot at x: %.1f, y: %.1f, z: %.1f, yaw: %.1f", x, y, z, yaw);
     initial_pose_pub.publish(init_pose);
 }
+bool MultiFloorNav::check_robot_pose(double ground_truth_x, double ground_truth_y, double ground_truth_yaw){
+    while (!received_amcl_pose);
+    
+    double amcl_x = curr_pose.pose.pose.position.x;
+    double amcl_y = curr_pose.pose.pose.position.y;
+    double diff_x = fabs(ground_truth_x - amcl_x);
+    double diff_y = fabs(ground_truth_y - amcl_y);
+    double linear_error = sqrt(pow(diff_x,2) + pow(diff_y,2)); //Find linear error
+    double amcl_yaw = tf::getYaw(curr_pose.pose.pose.orientation);
+    double angular_error = fabs(angles::normalize_angle(ground_truth_yaw-amcl_yaw));
+    ROS_INFO(
+        "[Multi Floor Nav] Current Robot Pose, x: %.2f, y: %.2f, yaw: %.2f",amcl_x, amcl_y, amcl_yaw);
+    ROS_INFO("[Multi Floor Nav] Linear Error: %.2f, Angular Error: %.2f",linear_error, angular_error);
+    if((linear_error < max_linear_error))
+        return true;
+    else 
+        return false;
+}
 
 void MultiFloorNav::execute(){
     switch(nav_state){
         case MultiFloorNav::State::INIT_POSE:
             init_pose(4.0,-5.0, 0.5, 0.0);
-            nav_state = MultiFloorNav::State::DONE;
+            nav_state = MultiFloorNav::State::CHECK_INITPOSE;
+            ros::Duration(1.0).sleep();
+            break;
+        case MultiFloorNav::State::CHECK_INITPOSE:
+            if(check_robot_pose(4.0, -5.0, 0.0)){
+                ROS_INFO("[Multi Floor Nav] Robot at correct pose");
+                nav_state = MultiFloorNav::State::DONE;
+            }
+            else{
+                ROS_INFO("[Multi Floor Nav] Setting initial pose failed. Will Retry");
+                nav_state = MultiFloorNav::State::INIT_POSE;
+            }
             break;
         case MultiFloorNav::State::DONE:
             ROS_INFO_ONCE("Robot arrived at Goal");
